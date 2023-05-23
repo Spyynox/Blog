be3975caceda8b89898238f50b4549ff69868f0b
Spyynox <maximembiyakiaku@gmail.com>
2023-05-23 20:31:11 +0200

    update User entity + add faker

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -39,6 +39,12 @@ class User
     #[ORM\Column]
     private ?\DateTimeImmutable $updatedAt = null;
 
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable;
+        $this->updatedAt = new \DateTimeImmutable;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
