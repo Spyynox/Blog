bb1b58c4acd269c54c3ae64203e2fedbdc084f0f
Spyynox <maximembiyakiaku@gmail.com>
2023-06-23 22:14:03 +0200

    Add all categories in sidebar

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CategoryController extends AbstractController
+{
+    public function list(CategoryRepository $categoryRepository,): Response
+    {
+        $categories = $categoryRepository->findBy([], ['title' => 'ASC']);
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+}
