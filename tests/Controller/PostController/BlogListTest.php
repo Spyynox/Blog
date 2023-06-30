80016962542b28593724e81f070cb02a2433d501
Spyynox <maximembiyakiaku@gmail.com>
2023-06-30 17:30:53 +0200

    Add blog list test

---
+++ tests/Controller/PostController/BlogListTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BlogListTest extends WebTestCase
+{
+    public function testBlogList(): void
+    {
+        $client = static::createClient();
+        $urlGenerator = $client->getContainer()->get('router');
+
+        $client->request(
+            Request::METHOD_GET,
+            $urlGenerator->generate('blog_list')
+        );
+
+        $this->assertResponseStatusCodeSame(200);
+
+        $this->assertSelectorExists('h1');
+        $this->assertSelectorTextContains('h1', 'Blogs');
+
+        $this->assertSelectorExists('.pagination');
+
+    }
+}
